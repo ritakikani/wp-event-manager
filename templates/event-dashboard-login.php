934e586d766687b85a1805478e2e4feb0d4905d1
Rita <WP7@192.168.1.105>
2019-08-09 18:32:14 +0530

    If login page url not added in settings, then in dashboard page need to set default login url- solved

--- templates/event-dashboard-login.php
+++ templates/event-dashboard-login.php
@@ -1,3 +1,3 @@
 <div id="event-manager-event-dashboard">
-	<p class="account-sign-in wpem-alert wpem-alert-info"><?php _e( 'You need to be signed in to manage your listings.', 'wp-event-manager' ); ?> <a href="<?php echo apply_filters( 'event_manager_event_dashboard_login_url', get_option('event_manager_login_page_url') ); ?>"><?php _e( 'Sign in', 'wp-event-manager' ); ?></a></p>
+	<p class="account-sign-in wpem-alert wpem-alert-info"><?php _e( 'You need to be signed in to manage your listings.', 'wp-event-manager' ); ?> <a href="<?php echo !empty(get_option('event_manager_login_page_url')) ? apply_filters( 'event_manager_event_dashboard_login_url', get_option('event_manager_login_page_url') ) : home_url().'/wp-login.php'; ?>"><?php _e( 'Sign in', 'wp-event-manager' ); ?></a></p>
 </div>
